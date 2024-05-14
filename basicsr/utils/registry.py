3f1e2c07d943cf53f51e8ec931cbda10cdb0bb40
Shmuel Ronen <80190186+ShmuelRonen@users.noreply.github.com>
2024-05-14 14:25:14 +0300

    In progress

--- basicsr/utils/registry.py
+++ basicsr/utils/registry.py
@@ -1,41 +1,61 @@
-# registry.py
-import logging
-
-# Setting up a logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.WARNING) 
-handler = logging.FileHandler('registry_warnings.log')
-handler.setLevel(logging.WARNING)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+#  Modified from: https://github.com/facebookresearch/fvcore/blob/master/fvcore/common/registry.py  # noqa: E501
 
 
-class Registry:
+class Registry():
     """
-    A registry to provide name -> object mapping, supporting third-party user custom modules.
+    The registry that provides name -> object mapping, to support third-party
+    users' custom modules.
+
+    To create a registry (e.g. a backbone registry):
+
+    .. code-block:: python
+
+        BACKBONE_REGISTRY = Registry('BACKBONE')
+
+    To register an object:
+
+    .. code-block:: python
+
+        @BACKBONE_REGISTRY.register()
+        class MyBackbone():
+            ...
+
+    Or:
+
+    .. code-block:: python
+
+        BACKBONE_REGISTRY.register(MyBackbone)
     """
 
     def __init__(self, name):
+        """
+        Args:
+            name (str): the name of this registry
+        """
         self._name = name
         self._obj_map = {}
 
     def _do_register(self, name, obj):
-        if name in self._obj_map:
-            warning_message = f"Warning: An object named '{name}' is already registered in '{self._name}' registry! Skipping duplicate registration."
-            print(warning_message)
-            logger.warning(warning_message)
-            return
+        assert (name not in self._obj_map), (f"An object named '{name}' was already registered "
+                                             f"in '{self._name}' registry!")
         self._obj_map[name] = obj
 
     def register(self, obj=None):
+        """
+        Register the given object under the the name `obj.__name__`.
+        Can be used as either a decorator or not.
+        See docstring of this class for usage.
+        """
         if obj is None:
+            # used as a decorator
             def deco(func_or_class):
                 name = func_or_class.__name__
                 self._do_register(name, func_or_class)
                 return func_or_class
+
             return deco
-    
+
+        # used as a function call
         name = obj.__name__
         self._do_register(name, obj)
 
