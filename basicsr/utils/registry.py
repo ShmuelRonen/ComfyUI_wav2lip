a80806440938d6d14c26d603ea8dd0973e81e795
Shmuel Ronen <80190186+ShmuelRonen@users.noreply.github.com>
2024-05-14 13:30:12 +0300

    Fix

--- basicsr/utils/registry.py
+++ basicsr/utils/registry.py
@@ -1,5 +1,4 @@
 # registry.py
-# Modified from: https://github.com/facebookresearch/fvcore/blob/master/fvcore/common/registry.py  # noqa: E501
 import logging
 
 # Setting up a logger
@@ -14,43 +13,29 @@ logger.addHandler(handler)
 
 class Registry:
     """
-    The registry that provides name -> object mapping, to support third-party
-    users' custom modules.
+    A registry to provide name -> object mapping, supporting third-party user custom modules.
     """
 
     def __init__(self, name):
-        """
-        Args:
-            name (str): the name of this registry
-        """
         self._name = name
         self._obj_map = {}
 
     def _do_register(self, name, obj):
         if name in self._obj_map:
-            warning_message = (f"Warning: An object named '{name}' is already registered in "
-                               f"'{self._name}' registry! Skipping duplicate registration.")
+            warning_message = f"Warning: An object named '{name}' is already registered in '{self._name}' registry! Skipping duplicate registration."
             print(warning_message)
             logger.warning(warning_message)
             return
         self._obj_map[name] = obj
 
     def register(self, obj=None):
-        """
-        Register the given object under the name `obj.__name__`.
-        Can be used as either a decorator or not.
-        See docstring of this class for usage.
-        """
         if obj is None:
-            # used as a decorator
             def deco(func_or_class):
                 name = func_or_class.__name__
                 self._do_register(name, func_or_class)
                 return func_or_class
-
             return deco
-
-        # used as a function call
+    
         name = obj.__name__
         self._do_register(name, obj)
 
@@ -60,5 +45,18 @@ class Registry:
             raise KeyError(f"No object named '{name}' found in '{self._name}' registry!")
         return ret
 
-    def __contains__(
+    def __contains__(self, name):
+        return name in self._obj_map
 
+    def __iter__(self):
+        return iter(self._obj_map.items())
+
+    def keys(self):
+        return self._obj_map.keys()
+
+
+DATASET_REGISTRY = Registry('dataset')
+ARCH_REGISTRY = Registry('arch')
+MODEL_REGISTRY = Registry('model')
+LOSS_REGISTRY = Registry('loss')
+METRIC_REGISTRY = Registry('metric')
