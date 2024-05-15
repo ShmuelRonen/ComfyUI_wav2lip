e38f515ecb75dd3994c7b150ec71e8ced467c2dc
Shmuel Ronen <80190186+ShmuelRonen@users.noreply.github.com>
2024-05-15 21:42:31 +0300

    fix

--- basicsr/losses/__init__.py
+++ basicsr/losses/__init__.py
@@ -2,20 +2,19 @@ from copy import deepcopy
 
 from custom_nodes.facerestore_cf.basicsr.utils import get_root_logger
 from custom_nodes.facerestore_cf.basicsr.utils.registry import LOSS_REGISTRY
-from .losses import (CharbonnierLoss, GANLoss, L1Loss, MSELoss, PerceptualLoss, WeightedTVLoss, g_path_regularize,
+from .losses import (Wav2LipCharbonnierLoss, Wav2LipGANLoss, Wav2LipL1Loss, Wav2LipMSELoss, Wav2LipPerceptualLoss, Wav2LipWeightedTVLoss, g_path_regularize,
                      gradient_penalty_loss, r1_penalty)
 
 __all__ = [
-    'L1Loss', 'MSELoss', 'CharbonnierLoss', 'WeightedTVLoss', 'PerceptualLoss', 'GANLoss', 'gradient_penalty_loss',
+    'Wav2LipL1Loss', 'Wav2LipMSELoss', 'Wav2LipCharbonnierLoss', 'Wav2LipWeightedTVLoss', 'Wav2LipPerceptualLoss', 'Wav2LipGANLoss', 'gradient_penalty_loss',
     'r1_penalty', 'g_path_regularize'
 ]
-
 
 def build_loss(opt):
     """Build loss from options.
 
     Args:
-        opt (dict): Configuration. It must constain:
+        opt (dict): Configuration. It must contain:
             type (str): Model type.
     """
     opt = deepcopy(opt)
