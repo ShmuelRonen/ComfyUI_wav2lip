529b2cf7a4475bde22fa4341d960aa8400003c11
GeekyGhost <111990299+GeekyGhost@users.noreply.github.com>
2024-06-27 22:22:28 -0400

    Update wav2lip_node.py

--- Wav2Lip/wav2lip_node.py
+++ Wav2Lip/wav2lip_node.py
@@ -15,7 +15,7 @@ def get_smoothened_boxes(boxes, T):
         boxes[i] = np.mean(window, axis=0)
     return boxes
 
-def create_smooth_mask(shape, padding=50):
+def create_smooth_mask(shape, padding=44):
     mask = np.ones(shape, dtype=np.float32)
     mask[:padding, :] = mask[-padding:, :] = mask[:, :padding] = mask[:, -padding:] = 0
     mask = cv2.GaussianBlur(mask, (0, 0), sigmaX=padding/2, sigmaY=padding/2)
